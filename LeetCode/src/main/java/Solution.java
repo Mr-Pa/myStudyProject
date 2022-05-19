d093ae42240d0dc65ec4cfaf7429dea611f804b6
Mr-Pa <1428042719@qq.com>
2022-05-20 00:49:16 +0800

    feat:LeetCode Solution 436. 寻找右区间

--- LeetCode/src/main/java/Solution.java
+++ LeetCode/src/main/java/Solution.java
@@ -1,4 +1,5 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Clay.Lin
@@ -6,9 +7,8 @@
  */
 public class Solution {
     public static void main(String[] args) {
-        int[] nums1 = new int[]{};
-        int[] nums2 = new int[]{2, 3};
-        System.out.println(findMedianSortedArrays(nums1, nums2));
+        int[][] intervals = new int[][]{{3,4},{2,3},{1,2}};
+        System.out.println(findRightInterval(intervals));
     }
 
 
@@ -35,7 +35,6 @@ public class Solution {
      * 链接：https://leetcode.cn/problems/median-of-two-sorted-arrays
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
-
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
@@ -98,6 +97,76 @@ public class Solution {
         }
 
         return (double) count;
+    }
+
+    /**
+     * 436. 寻找右区间
+     * 给你一个区间数组 intervals ，其中 intervals[i] = [starti, endi] ，且每个 starti 都 不同 。
+     * <p>
+     * 区间 i 的 右侧区间 可以记作区间 j ，并满足 startj >= endi ，且 startj 最小化 。
+     * <p>
+     * 返回一个由每个区间 i 的 右侧区间 的最小起始位置组成的数组。如果某个区间 i 不存在对应的 右侧区间 ，则下标 i 处的值设为 -1 。
+     * <p>
+     *  
+     * 示例 1：
+     * <p>
+     * 输入：intervals = [[1,2]]
+     * 输出：[-1]
+     * 解释：集合中只有一个区间，所以输出-1。
+     * 示例 2：
+     * <p>
+     * 输入：intervals = [[3,4],[2,3],[1,2]]
+     * 输出：[-1,0,1]
+     * 解释：对于 [3,4] ，没有满足条件的“右侧”区间。
+     * 对于 [2,3] ，区间[3,4]具有最小的“右”起点;
+     * 对于 [1,2] ，区间[2,3]具有最小的“右”起点。
+     * 示例 3：
+     * <p>
+     * 输入：intervals = [[1,4],[2,3],[3,4]]
+     * 输出：[-1,2,-1]
+     * 解释：对于区间 [1,4] 和 [3,4] ，没有满足条件的“右侧”区间。
+     * 对于 [2,3] ，区间 [3,4] 有最小的“右”起点。
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= intervals.length <= 2 * 104
+     * intervals[i].length == 2
+     * -106 <= starti <= endi <= 106
+     * 每个间隔的起点都 不相同
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/find-right-interval
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param intervals
+     * @return
+     */
+    public static int[] findRightInterval(int[][] intervals) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] sort = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            //记录各个start的下标
+            map.put(intervals[i][0], i);
+            sort[i] = intervals[i][0];
+        }
+        //对所有区间的start进行排序
+        SortUtil.insertionSort(sort);
+
+        int[] result = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            //获取大于end的最小start，可以用查找算法优化
+            Integer minStart = null;
+            for (int j = 0; j < sort.length; j++) {
+                if (sort[j] >= intervals[i][1]){
+                    minStart = sort[j];
+                    break;
+                }
+            }
+            result[i] = minStart != null ? map.get(minStart) : -1;
+        }
+        return result;
     }
 
 
